bed8851ec9ba1fb8217d30141c565c5c0578a640
Alec Aoki <alec.camposaoki@gmail.com>
2024-10-29 18:35:27 -0300

    Add selectionsort

--- lib/impl/selection.c
+++ lib/impl/selection.c
@@ -2,6 +2,30 @@
 
 Data selectionSort(int *array, int size) {
   INIT_DATA(data);
+  data.start = clock();
+
+  int lowestPos = 0, aux;
+
+  for(int i=0; i<size-1; i++){
+      lowestPos = i;
+
+      for(int j=i+1; j<size; j++){
+          if(array[j] < array[lowestPos]){
+              lowestPos = j;
+          }
+          data.numberOfComparisons++;
+      }
+
+      if(lowestPos != i){
+          aux = array[i];
+          array[i] = array[lowestPos];
+          array[lowestPos] = aux;
+          data.numberOfSwaps++;
+      }
+  }
+
+  data.end = clock();
+  data.time = (double)(data.end - data.start) / CLOCKS_PER_SEC;
 
   return data;
 }
